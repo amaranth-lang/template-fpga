4c493bb69b9e21238ad9ad86f3aa261c7bb6411e
Catherine <whitequark@whitequark.org>
2023-07-21 04:33:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# amaranth-template-fpga
+
+TODO: write this section
+
+```shell
+$ pipx install pdm
+$ pdm install
+$ pdm run build_ice40
+$ pdm run build_ecp5
+```
---
+++ src/amaranth_template_fpga.py
@@ -0,0 +1,27 @@
+from amaranth import *
+from amaranth_boards.icestick import ICEStickPlatform
+from amaranth_boards.versa_ecp5 import VersaECP5Platform
+
+
+class Blinky(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+
+        led = platform.request("led", 0).o
+        timer = Signal(range(int(platform.default_clk_frequency//2)),
+                       reset=int(platform.default_clk_frequency//2) - 1)
+        with m.If(timer == 0):
+            m.d.sync += led.eq(~led)
+            m.d.sync += timer.eq(timer.reset)
+        with m.Else():
+            m.d.sync += timer.eq(timer - 1)
+
+        return m
+
+
+def build_ice40():
+    ICEStickPlatform().build(Blinky())
+
+
+def build_ecp5():
+    VersaECP5Platform().build(Blinky())
